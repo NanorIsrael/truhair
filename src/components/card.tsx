ba76b85a97c0f2aefc2ee769ad576f492a8b9e42
nanorisrael <israel.nanor@compozelabs.com>
2022-10-18 08:55:55 +0000

    changed booknow to use grid

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,9 +1,11 @@
 const  Card = ({img}: any) => (
-    <div>
-    <div className="bg-white rounded overflow-hidden shadow-md w-80 h-80 m-2 lg:w-64 m-1">
-    <img src={`/images/hair-styles/` + img.photo} alt='' className='w-full h-full'/>
-    </div>
-    <p>{img.name}</p>
+    <div className="mb-8 mt-16">
+        <div className="bg-white rounded overflow-hidden shadow-md w-100 h-80 m-2 lg:w-64 m-1 ">
+            <img src={`/images/hair-styles/` + img.photo} alt='' className='w-full h-full object-cover'/>
+        </div>
+        <p>{img.name}</p>
     </div>
 )
 export default Card;
+
+
