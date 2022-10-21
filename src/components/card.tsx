efd248609f482dc46418a68c33027495aa3c43ba
nanorisrael <israel.nanor@compozelabs.com>
2022-10-21 19:27:57 +0000

    On branch hover_book_now_card added  hovering

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,11 +1,21 @@
-const  Card = ({img}: any) => (
-    <div className="mb-6 mt-4">
-        <div className="bg-white rounded overflow-hidden shadow-md w-100 h-80 m-2 lg:w-64 m-1 ">
+
+
+
+
+const  Card = ({img, onHover}: any) => {
+
+    return (
+    <div className="card mb-6 m-4" onMouseOver={onHover}>
+        <div className="card-image bg-white rounded overflow-hidden shadow-md w-100 h-80 m-2 lg:w-64 m-1 ">
             <img src={`/images/hair-styles/` + img.photo} alt='' className='w-full h-full object-cover'/>
         </div>
-        <p>{img.name}</p>
+        <div className="item-info" >
+            <p>{img.name}</p>
+        </div>
+       
     </div>
-)
+    )
+}
 export default Card;
 
 
--- src/pages/BookNow.tsx
+++ src/pages/BookNow.tsx
@@ -1,21 +1,37 @@
 import Card from "../components/card";
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 // flex justify-center flex-wrap align-center
 
 function BookNow() {
-    const images = [{photo:'Butterfly-braids.jpeg', name:'Butterfly Braids'}, {photo:'crocheting.jpg', name:'Crocheting'}, {photo:'distress-locks.jpg', name:'Distress-Locks'}, {photo:'gel-pony.jpg', name:'Gel Pony'}, {photo:'corn-row.jpg', name:'Corn Row'}, {photo:'boho.jpg', name:'Boho'}]
+    const images = [{photo:'Butterfly-braids.jpeg', name:'Butterfly Braids'}, {photo:'crocheting.jpg', name:'Crocheting'}, {photo:'distress-locks.jpg', name:'Distress-Locks'}, {photo:'gel-pony.jpg', name:'Gel Pony'}, {photo:'corn-row.jpg', name:'Corn Row'}, {photo:'boho.jpg', name:'Boho'}, {photo:'Butterfly-braids.jpeg', name:'Butterfly Braids'}, {photo:'crocheting.jpg', name:'Crocheting'}, {photo:'distress-locks.jpg', name:'Distress-Locks'}]
     const [visible, setVisible] = useState(3);
+    // const [card, setCard] = useState()
+
+    useEffect(() => {
+        // const card = document.querySelector(".card");
+        // console.log(card)
+
+        // card?.addEventListener('click', () => {
+        //     console.log(card)
+
+        // })
+    }, [])
 
     const loadMore = () => {
         setVisible(prev => prev + 3)
     }
+
+    const handleMouseOver = (event: any) => {
+        console.log(event.target)
+        // event.target.style.positon = 'relative'
+    }
    
     return (
-    <div className='bg-primary text-center text-white' >
+    <div className='bg-primary-color-light text-center primary-dark' >
         <div className="item-center w-90 block-center">
             <p className="text-center pt-24 text-2xl pb-8 md:pt-32">Click on the service you want, to book <strong>now!</strong></p>
             <div className="grid-cta">
-                { images.slice(0, visible).map(img => <Card img={img}/>)}
+                { images.slice(0, visible).map((img, index) => <Card img={img} key={index} onHover={handleMouseOver}/>)}
             </div>
             <button onClick={loadMore} className="btn mb-8 primary-color bg-primary-dark w-full py-7" >Load More</button>
         </div>
